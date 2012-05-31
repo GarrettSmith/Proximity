330056bc13b2876a0d9d819f1e198f46d829c350
Garrett Smith <garrettwhsmith@gmail.com>
2012-05-31 14:01:45 -0500

    Placeholder tests

---
+++ test/ca/uwinnipeg/proximity/PerceptualSystemTest.java
@@ -0,0 +1,114 @@
+package ca.uwinnipeg.proximity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PerceptualSystemTest {
+
+  @Test
+  public void testPerceptualSystem() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testPerceptualSystemProbeFuncOfOArrayBoolean() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testGetDescriptionBasedNeighbourhood() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testEqual() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testGetHybridNeighbourhood() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testDistance() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testGetDescriptionBasedIntersect() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testDescriptionBasedNear() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testDescriptionBasedDegree() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testGetHybridIntersect() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testHybridNear() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testHybridDegree() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testGetCachedDescriptions() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testGetDescription() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testGetObjects() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testGetRegions() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testAddRegion() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testRemoveRegion() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testGetProbeFuncs() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testAddProbeFunc() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testRemoveProbeFunc() {
+    fail("Not yet implemented"); // TODO
+  }
+
+}
